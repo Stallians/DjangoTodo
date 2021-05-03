8c417afcd6e8a872c0c2027e55c7608514c96f11
Ashish <ashish.agrawal1502@gmail.com>
2021-05-04 00:29:46 +0530

    added api to get task

--- todoapp/todo/urls.py
+++ todoapp/todo/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r"tasks", views.TaskViewSet)
+
 
 urlpatterns = [
     path("", views.alltasks, name="alltasks"),
+    path("api/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("add/", views.add, name="add_task"),
     path("toggle/<int:taskid>", views.toggle_status, name="toggle_status"),
     path("delete/<int:taskid>", views.delete, name="delete_task"),
--- todoapp/todo/views.py
+++ todoapp/todo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, reverse, get_object_or_404
 from django.http import HttpResponseRedirect
+from rest_framework import viewsets
+from .serializers import TaskSerializer
 from .models import Task
 
 
@@ -45,3 +47,8 @@ def delete(request, taskid):
     task = get_object_or_404(Task, id=taskid)
     task.delete()
     return HttpResponseRedirect(reverse("alltasks"))
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
